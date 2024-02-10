8a6a5faa5e504b14ba4ead19c87f4c343f38ceed
aescorcia05 <156947111+aescorcia05@users.noreply.github.com>
2024-02-10 17:43:06 -0500

    Better late than never

--- MarioKartGame.java
+++ MarioKartGame.java
@@ -1,1 +1,49 @@
+//Create a Java class called MarioKartGame with the following:
 
+import java.util.Scanner;
+
+public class MarioKartGame {
+
+//In the main method:
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String temporaryName;
+        String temporaryCharacter;
+        int temporarySpeed;
+
+        System.out.println("Player 1");
+
+        System.out.println("Name: ");
+        temporaryName = input.next();
+        System.out.println("Character: ");
+        temporaryCharacter = input.next();
+        System.out.println("Speed: ");
+        temporarySpeed = input.nextInt();
+
+        MarioKartPlayer player1 = new MarioKartPlayer(temporaryName, temporaryCharacter, temporarySpeed);
+
+        System.out.println("Player 2");
+
+        System.out.println("Name: ");
+        temporaryName = input.next();
+        System.out.println("Character: ");
+        temporaryCharacter = input.next();
+        System.out.println("Speed: ");
+        temporarySpeed = input.nextInt();
+
+        MarioKartPlayer player2 = new MarioKartPlayer(temporaryName, temporaryCharacter, temporarySpeed);
+
+        player1.displayInfo();
+        player2.displayInfo();
+
+        player1.boost();
+        player2.boost();
+
+        player1.displayInfo();
+        player2.displayInfo();
+
+    }
+
+
+}
--- MarioKartPlayer.java
+++ MarioKartPlayer.java
@@ -1,1 +1,27 @@
+//Define a Java class called MarioKartPlayer with the following attributes:
 
+public class MarioKartPlayer {
+    // Attributes
+    String playerName;
+    String character;
+    int speed;
+
+    // Constructor
+    public MarioKartPlayer(String name, String character, int startingSpeed) {
+        this.playerName = name;
+        this.character = character;
+        this.speed = startingSpeed;
+    }
+
+    // Methods
+    public void boost() {
+        speed += 10;
+    }
+
+    public void displayInfo() {
+        System.out.println("Player name: " + playerName);
+        System.out.println("Character: " + character);
+        System.out.println("Speed: " + speed);
+    }
+
+}
